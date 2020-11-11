94078225a191fae3f3ac100228c319c47c7352fc
e-floran <e-floran@hotmail.fr>
2020-11-11 20:08:12 +0100

    props passées à person et création de deuxième personne

--- people-of-europe/src/components/Person.jsx
+++ people-of-europe/src/components/Person.jsx
@@ -1,9 +1,10 @@
 export default function person(props){
     const{firstName, lastName, pic, city, country} = props;
+    const fullName = firstName+" "+lastName
     return(
         <div>
-            <h3>{firstName} {lastName}</h3>
-            <img src={pic} alt=`${firstName} ${lastName}`/>
+            <h3>{fullName}</h3>
+            <img src={pic} alt={fullName}/>
             <p>{city}, {country}</p>
         </div>
     )
