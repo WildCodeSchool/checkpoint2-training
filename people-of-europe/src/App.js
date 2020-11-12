498d0a694d43841bf70b4f7352b755bc58cf5325
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-12 08:36:37 +0100

    Create components Header, Person and Footer

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,20 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Person />
+      <Footer />
     </div>
   );
 }
---
+++ people-of-europe/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2020 Your Name</p>
+    </footer>
+  )
+}
+
+export default Footer;
