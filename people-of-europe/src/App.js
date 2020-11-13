123c5fa5c64d5b515728b8c3e26bca634b97de26
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-13 10:57:59 +0100

    Clean-up console.log's

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -22,7 +22,6 @@ class App extends Component {
         return response.data;
       })
       .then(persons => {
-        console.log('change state', persons);
         this.setState({
           persons: persons
         });
@@ -30,13 +29,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('did mount');
     this.fetchPersons();
   }
 
   render() {
     const { persons } = this.state;
-    console.log('render', persons);
     return (
       <div className="App">
         <Header title="People of Europe" />
