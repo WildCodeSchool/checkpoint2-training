93c50f48cf6988f85c8e5814c2ca62c0f2f85a83
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-13 09:52:14 +0100

    Fetch persons from API using axios

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,15 +1,44 @@
+import { Component } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PersonList from './components/PersonList';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header title="People of Europe" />
-      <PersonList />
-      <Footer year="2020" authorName="Benoît Hubert" />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: []
+    };
+  }
+
+  fetchPersons = () => {
+    // axios.get => lance la requête
+    axios.get('https://my-json-server.typicode.com/bhubr/people-api/people')
+      // .then (then = alors/ensuite) => spécifier ce que je dois faire quand
+      // je reçois la réponse du serveur
+      .then((response) => {
+        return response.data;
+      })
+      .then(persons => {
+        this.setState({
+          persons: persons
+        });
+      });
+  }
+
+  render() {
+    const { persons } = this.state;
+    return (
+      <div className="App">
+        <Header title="People of Europe" />
+        <PersonList persons={persons} />
+        <button type="button" onClick={this.fetchPersons}>Fetch persons</button>
+        <Footer year="2020" authorName="Benoît Hubert" />
+      </div>
+    );
+  }
 }
 
 export default App;
