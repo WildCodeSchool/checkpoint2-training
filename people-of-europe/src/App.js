accaf343a1d61c9648e06d7eb7af3c5968814756
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-13 08:56:04 +0100

    Repeat Person component from PersonList

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,25 +1,12 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 function App() {
   return (
     <div className="App">
       <Header title="People of Europe" />
-      <Person
-        firstName="John"
-        lastName="Wayne"
-        pic="https://upload.wikimedia.org/wikipedia/commons/7/7b/John_Wayne_-_still_portrait.jpg"
-        city="Winterset"
-        country="United States"
-      />
-      <Person
-        firstName="Mary"
-        lastName="Wayne"
-        pic="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Missing_avatar.svg/200px-Missing_avatar.svg.png"
-        city="Helsinki"
-        country="Finland"
-      />
+      <PersonList />
       <Footer year="2020" authorName="Benoît Hubert" />
     </div>
   );
