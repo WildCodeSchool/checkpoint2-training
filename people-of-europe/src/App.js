eb7dc670272af9cc9486b099324ca4ca5075cab8
Cédric Priouret <cedric.priouret@gmail.com>
2020-11-14 10:27:03 +0100

    add components Footer.jsx

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Person from "./components/Person";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Person />
+      <Footer />
     </div>
   );
 }
---
+++ people-of-europe/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p>© 2020 Cédric PRIOURET</p>
+    </div>
+  );
+}
+
+export default Footer;
