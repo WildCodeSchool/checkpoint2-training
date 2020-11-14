f327dab94bf92724d4420e7029d2c182d15e57e0
Cédric Priouret <cedric.priouret@gmail.com>
2020-11-14 10:20:36 +0100

    add components Person.jsx

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Person from "./components/Person";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Person />
     </div>
   );
 }
---
+++ people-of-europe/src/components/Person.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Person() {
+  return (
+    <div>
+      <h3>Foo Bar</h3>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Missing_avatar.svg/200px-Missing_avatar.svg.png"
+        alt="foo bar"
+      />
+      <p>Paris, France</p>
+    </div>
+  );
+}
+
+export default Person;
