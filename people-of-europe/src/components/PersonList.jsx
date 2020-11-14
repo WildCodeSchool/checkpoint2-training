8407d88b1b946876d23848539cdf9cd32db856b6
Cédric Priouret <cedric.priouret@gmail.com>
2020-11-14 15:42:02 +0100

    start filtering person with console.log

--- people-of-europe/src/components/PersonList.jsx
+++ people-of-europe/src/components/PersonList.jsx
@@ -3,6 +3,16 @@ import Person from "./Person";
 
 class PersonList extends React.Component {
   render() {
+    const selectedGender = "female";
+    const selectNat = "British";
+    const selectName = "Rosie";
+    const filteredData = this.props.person.filter(
+      (person) =>
+        person.gender === selectedGender &&
+        person.nat === selectNat &&
+        person.firstName === selectName
+    );
+    console.log("test", filteredData);
     return (
       <div>
         {this.props.person.map((person) => (
