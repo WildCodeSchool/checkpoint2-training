ceb33de2021784d554a07ac49d1ebf40518b3404
Cédric Priouret <cedric.priouret@gmail.com>
2020-11-15 16:09:59 +0100

    Add select for gender, nationality and input for name

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -9,6 +9,8 @@ class App extends React.Component {
   state = {
     person: [],
     prenom: '',
+    gender: 'default',
+    nat: 'default',
   };
 
   componentDidMount() {
@@ -31,7 +33,7 @@ class App extends React.Component {
   };
 
   render() {
-    const { prenom, person } = this.state;
+    const { prenom, person, gender, nat } = this.state;
     let filteredData = person;
     if (prenom.length !== 0) {
       filteredData = filteredData.filter((person) =>
@@ -39,10 +41,42 @@ class App extends React.Component {
       );
     }
 
+    if (gender !== 'default') {
+      filteredData = filteredData.filter(
+        (person) => person.gender.toLowerCase() === gender.toLowerCase()
+      );
+    }
+
+    if (nat !== 'default') {
+      filteredData = filteredData.filter(
+        (person) => person.nat.toLowerCase() === nat.toLowerCase()
+      );
+    }
+
+    console.log(gender);
     return (
       <div className="App">
         <Header title="Meetic Europe" />
+        <label>Choose a filter : </label>
+        <br />
+        <label>Name : </label>
         <input name="prenom" value={prenom} onChange={this.handleChange} />
+        <br />
+        <label>Gender : </label>
+        <select name="gender" onChange={this.handleChange}>
+          <option value="default">-Please choose an option-</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+        <br />
+        <label>Nationality : </label>
+        <select name="nat" onChange={this.handleChange}>
+          <option value="default">-Please choose an option-</option>
+          <option value="british">British</option>
+          <option value="french">French</option>
+          <option value="german">German</option>
+          <option value="spanish">Spanish</option>
+        </select>
         <PersonList person={filteredData} />
         <Footer year="2020 " authorName="© Cédric Priouret" />
       </div>
