88a0a2942d98c15d7950fea28e0f69efb11660ad
CASTAN <christophecastan4@gmail.com>
2020-11-15 18:31:57 +0100

    suite footer

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,14 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-git;
