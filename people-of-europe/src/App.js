1d34e2008c846059f99bd1a5c6f699db16ccd68d
CASTAN <christophecastan4@gmail.com>
2020-11-15 19:55:26 +0100

    component Person

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Person from "./components/Person";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <Person />
     </div>
   );
 }
---
+++ people-of-europe/src/components/Person.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Person() {
+  return (
+    <div className="Person">
+      <h3>Foo Bar</h3>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Missing_avatar.svg/200px-Missing_avatar.svg.png"
+        alt="Foo Bar"
+      />
+      <p>Paris, France</p>
+    </div>
+  );
+}
+
+export default Person;
