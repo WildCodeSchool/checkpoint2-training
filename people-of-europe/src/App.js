c55f0c95a1456c1460aa7190f1f12441b553d4fa
Miguel Santos <migueljsantos27@gmail.com>
2020-11-15 15:37:12 +0100

    add api

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,16 +1,43 @@
+import { Component } from "react";
 import "./App.css";
+import axios from "axios";
 import Header from "./components/Header";
 import PersonList from "./components/PersonList";
 import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <Header title="React is a nightmare" />
-      <PersonList />
-      <Footer year="2020" authorName="Miguel Santos" />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [],
+    };
+  }
 
+  fetchPersons = () => {
+    axios
+      .get("https://my-json-server.typicode.com/bhubr/people-api/people")
+      .then((response) => {
+        return response.data;
+      })
+      .then((persons) => {
+        this.setState({
+          persons: persons,
+        });
+      });
+  };
+
+  render() {
+    const { persons } = this.state;
+    return (
+      <div className="App">
+        <Header title="React is a nightmare" />
+        <PersonList persons={persons} />
+        <button type="button" onClick={this.fetchPersons}>
+          Fetch persons
+        </button>
+        <Footer year="2020" authorName="Miguel Santos" />
+      </div>
+    );
+  }
+}
 export default App;
