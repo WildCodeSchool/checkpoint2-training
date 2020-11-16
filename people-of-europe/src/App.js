dd8e9ab2d1a9461993ad8ea328fc4dc706bf0e75
CASTAN <christophecastan4@gmail.com>
2020-11-16 11:59:51 +0100

    repeat component

--- people-of-europe/src/App.js
+++ people-of-europe/src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
   return (
     <div className="App">
       <Header title="People of Europe" />
       <Footer year="2020" authorName="Christophe Castan" />
-      <Person
-        firstName="Cristiano"
-        lastName="Ronaldo"
-        pic="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
-        city="Funchal"
-        country="Portugal"
-      />
-      <Person
-        firstName="Renaud"
-        lastName="Ripart"
-        pic="https://www.sofoot.com/IMG/joueurs/renaud-ripart-150441.jpg"
-        city="Nîmes"
-        country="France"
-      />
+      <PersonList />
     </div>
   );
 }
---
+++ people-of-europe/src/components/PersonList.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Person from "./Person";
+import "./PersonList.css";
+
+const samplePersons = [
+  {
+    id: 3,
+    firstName: "Léa",
+    lastName: "Marchand",
+    gender: "female",
+    loc: {
+      city: "Dunkerque",
+      country: "France",
+    },
+    email: "lea.marchand@example.com",
+    pic: "https://randomuser.me/api/portraits/women/0.jpg",
+    nat: "French",
+  },
+  {
+    id: 4,
+    firstName: "Matteo",
+    lastName: "Michel",
+    gender: "male",
+    loc: {
+      city: "Boulogne-Billancourt",
+      country: "France",
+    },
+    email: "matteo.michel@example.com",
+    pic: "https://randomuser.me/api/portraits/men/46.jpg",
+    nat: "French",
+  },
+  {
+    id: 5,
+    firstName: "Anett",
+    lastName: "Schenkel",
+    gender: "female",
+    loc: {
+      city: "Lennestadt",
+      country: "Germany",
+    },
+    email: "anett.schenkel@example.com",
+    pic: "https://randomuser.me/api/portraits/women/36.jpg",
+    nat: "German",
+  },
+];
+
+function PersonList() {
+  return (
+    <div className="PersonList">
+      {samplePersons.map((singlePerson) => {
+        return (
+          <Person
+            key={singlePerson.id}
+            firstName={singlePerson.firstName}
+            lastName={singlePerson.lastName}
+            pic={singlePerson.pic}
+            city={singlePerson.loc.city}
+            country={singlePerson.loc.country}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default PersonList;
